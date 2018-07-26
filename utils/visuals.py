9dcd105cc630489363a07dd44a9bffddc852684d
hkailee <leehongkai@gmail.com>
2018-07-26 22:05:18 +0800

    update

---
+++ utils/visuals.py
@@ -0,0 +1,53 @@
+###########################################
+# Suppress matplotlib user warnings
+# Necessary for newer version of matplotlib
+import warnings
+warnings.filterwarnings("ignore", category = UserWarning, module = "matplotlib")
+#
+# Display inline matplotlib plots with IPython
+from IPython import get_ipython
+get_ipython().run_line_magic('matplotlib', 'inline')
+###########################################
+
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import pandas as pd
+import numpy as np
+
+def pca_results(good_data, pca):
+	'''
+	Create a DataFrame of the PCA results
+	Includes dimension feature weights and explained variance
+	Visualizes the PCA results
+	'''
+
+	# Dimension indexing
+	dimensions = dimensions = ['Dimension {}'.format(i) for i in range(1,len(pca.components_)+1)]
+
+    # PCA components
+	components = pd.DataFrame(np.round(pca.components_, 4), columns = good_data.keys())
+	components.index = dimensions
+
+	# PCA explained variance
+	ratios = pca.explained_variance_ratio_.reshape(len(pca.components_), 1)
+	variance_ratios = pd.DataFrame(np.round(ratios, 4), columns = ['Explained Variance'])
+	variance_ratios.index = dimensions
+
+	# Create a bar plot visualization
+	fig, ax = plt.subplots(figsize = (14,8))
+
+	# Plot the feature weights as a function of the components
+	components.plot(ax = ax, kind = 'bar');
+	ax.set_ylabel("Feature Weights")
+	ax.set_xticklabels(dimensions, rotation=0)
+
+
+	# Display the explained variance ratios
+	for i, ev in enumerate(pca.explained_variance_ratio_):
+		ax.text(i-0.40, ax.get_ylim()[1] + 0.05, "Explained Variance\n          %.4f"%(ev))
+
+	# Return a concatenated DataFrame
+	return pd.concat([variance_ratios, components], axis = 1)
+
+    
+
